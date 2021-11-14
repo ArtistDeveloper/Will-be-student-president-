74cd2d3de9712fcf3a224a06e206c9ec8efb2baa
Artiper <somadubel@gmail.com>
2021-11-14 21:05:06 +0900

    UPDATE : 폴더정리 및 텍스트필드 UI반응형 추가

--- Assets/02. Scripts/Mesaage/MessageSystem.cs
+++ Assets/02. Scripts/Mesaage/MessageSystem.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class MessageSystem : MonoBehaviour
 {
-    // 가비지 생기니 string buffer 사용해볼까. 일단 생산성이 중요하니 DoTween에셋을 써보자
     string testText = "엄청나게 긴 텍스트 ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ";
 
     private void Start()
