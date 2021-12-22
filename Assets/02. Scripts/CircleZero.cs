4d81f899eb2812ba68ede1c4a715c964b8524b89
Circlezero <wy129@naver.com>
2021-12-22 19:38:25 +0900

    ADD, EDIT
    ADD : PanelToggleControl, DebugButton
    EDIT : HappeningOccur
    디버그용 이벤트 출력 버튼 만들고, HappeningOccur 클래스 정상작동하도록 수정

--- Assets/02. Scripts/CircleZero.cs
+++ Assets/02. Scripts/CircleZero.cs
@@ -7,7 +7,7 @@ public class CircleZero : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // HappeningUtils.happeningUtils.ReadData(); 이렇게 쓰면 될듯?
     }
 
     // Update is called once per frame
