008dcfd0d20c2071a355fdca1df0e7fbcf596b58
Artiper <somadubel@gmail.com>
2022-01-31 05:28:42 +0900

    FIX, UPDATE : 스탯 UI 애니메이션 자연스럽게 로직 수정
    FIX
    - 마우스 클릭 끝까지 했을 때 다시 돌아오는 예외처리 (+)
    
    UPDATE
    -  스탯 UI 애니메이션 alpha값 조절 자연스럽게 바꾸기

--- Assets/02. Scripts/UI/StatusCharging.cs
+++ Assets/02. Scripts/UI/StatusCharging.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StatusAnimation : MonoBehaviour
+public class StatusCharging : MonoBehaviour
 {
-    public Image img;
+    private Image img;
     [HideInInspector] public int stat;
 
     private const int statMax = 100;
 
     private void Start()
     {
+        img = GetComponent<Image>();
         img.fillAmount = 0f;
         img.type = Image.Type.Filled;
         img.fillMethod = Image.FillMethod.Vertical;
         img.fillOrigin = (int)Image.OriginVertical.Bottom;
     }
 
-    // private void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Tab))
-    //     {
-    //         StartCoroutine(PlayStatAnim(30f, 70f));
-    //     }
-    // }
+    private void Update()
+    {
+        // Debug.Log(Time.time);
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            StartCoroutine(CoChargeStatAnim(30f, 70f));
+        }
+    }
 
 
     /// <summary>
@@ -33,23 +36,24 @@ public class StatusAnimation : MonoBehaviour
     /// <param name="startVal"></param> 값
     /// <param name="goalVal"></param>
     /// <returns></returns>
-    private IEnumerator PlayStatAnim(float startVal, float goalVal)
+    private IEnumerator CoChargeStatAnim(float startVal, float goalVal)
     {
         float changeVal = startVal;
         float elapsedTime = 0;
         float progress = 0;
 
+        // 이걸로 몇초가 걸리는진 알 수 없는게, progress에 따라 보간값이 바뀌는 것이기 때문.
         while (changeVal <= goalVal)
         {
             // changeVal = Mathf.Lerp((float)changeVal, (float)goalVal, Time.smoothDeltaTime / 2.5f);
             changeVal = Mathf.Lerp((float)changeVal, (float)goalVal, progress);
             elapsedTime += Time.unscaledDeltaTime;
-            progress = elapsedTime / 5f;
+            progress = elapsedTime / 100f;
 
-            Debug.Log("changeVal : " + changeVal);
+            // Debug.Log("changeVal : " + changeVal);
 
             img.fillAmount = changeVal / 100f;
-            Debug.Log("img.fillAmount : " + img.fillAmount);
+            // Debug.Log("img.fillAmount : " + img.fillAmount);
 
             yield return null;
         }
